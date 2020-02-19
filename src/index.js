08c85a0b9c97f6a374b43a72dca73f2b75caab56
blockems <block@as.net.au>
2020-02-19 15:22:17 +1100

    after adding envs and logging

--- src/index.js
+++ src/index.js
@@ -1,32 +1,46 @@
-// importing the dependencies
+//web server
 const express = require('express');
+const app = express();
+
+//Converts an incoming JSON body to readable objects
 const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+// Security
 const cors = require('cors');
+app.use(cors());
+
+// securisation of express
 const helmet = require('helmet');
+app.use(helmet());
+
+// http monitoring
 const morgan = require('morgan');
-//const winston = require('winston');
+app.use(morgan('combined'));
+
+// loging (winston) plus custom formats for CREn
+// Change formats in the logging class
 const Logger = require('../services/logger_service');
 const logger = new Logger('app');
 
-// defining the Express app
-const app = express();
+// Command Line Variables (by position)
+//    (1) port: Tell the server which port to run on
+//    (2) debuglevel: to set the level of logging [1-5]
+//    (3) environment : what environment we are runnign in
+//
+const myArgs = process.argv.slice(2);
+const serverPort = myArgs[0];
+const debugLevel = myArgs[1];
+const envLevel = myArgs[2];
+
+console.log(serverPort);
+console.log(debugLevel);
+console.log(envLevel);
 
 // defining an array to work as the database (temporary solution)
 const ads = [
   {title: 'Hello, world (again)!'}
 ];
-
-// adding Helmet to enhance your API's security
-app.use(helmet());
-
-// using bodyParser to parse JSON bodies into JS objects
-app.use(bodyParser.json());
-
-// enabling CORS for all requests
-app.use(cors());
-
-// adding morgan to log HTTP requests
-app.use(morgan('combined'));
 
 // defining an endpoint to return all ads
 app.get('/', (req, res) => {
